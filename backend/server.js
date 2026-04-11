387d370eafa95ec1088354676044821d1db7279c
Hope_m@k--life coder <anegash880@gmail.com>
2026-04-11 05:31:27 -0700

    Clean project setup with gitignore (removed node_modules)
    # Conflicts:
    #	.gitignore
    #	backend/package.json
    #	backend/server.js

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
@@ -30,3 +31,31 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+=======
+// backend/server.js
+
+require("dotenv").config();
+const express = require("express");
+
+
+const errorHandler = require("./middleware/error.middleware");
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+
+// Test Route
+app.get("/", (req, res) => {
+  res.send("API running...");
+});
+
+// Error Middleware (must be last)
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+>>>>>>> 41ad3b9 (Clean project setup with gitignore (removed node_modules))
---
+++ backend/utils/response.js
@@ -0,0 +1,11 @@
+// backend/utils/response.js
+
+const sendResponse = (res, statusCode, success, message, data = null) => {
+  return res.status(statusCode).json({
+    success,
+    message,
+    data,
+  });
+};
+
+module.exports = sendResponse;
